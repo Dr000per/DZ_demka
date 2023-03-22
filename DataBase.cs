f4aacc8499b59beac740251bc0732f36714a4971
German Shelyakin <wf.pro03@mail.ru>
2023-03-22 22:48:13 +0300

    Добавьте файлы проекта.

---
+++ DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DZ_demka
+{
+    internal class DataBase
+    {
+        SqlConnection db_con = new SqlConnection(@"Data Source=DESKTOP-BBAJL13\SQLEXPRESS;Initial Catalog= DZ_Demka; Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (db_con.State == System.Data.ConnectionState.Closed)
+            {
+                db_con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (db_con.State == System.Data.ConnectionState.Open)
+            {
+                db_con.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return db_con;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DZ_demka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_start = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(303, 266);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(157, 64);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "Запускай шарманку";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(154, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(461, 194);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.PageTabList;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
